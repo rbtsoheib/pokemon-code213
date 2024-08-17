90ffdfb95e6878dfae0a5b2282c986606d36479b
rbtsoheib <taharsoheib08@gmail.com>
2024-08-17 12:50:40 +0200

    commit

---
+++ crypto-essay-project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Components/Header';
+import CryptoList from './Components/CryptoList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <CryptoList />
+    </div>
+  );
+};
+export default App;
